0dbb3a410091dccb3ff6c6e1ab32e5d5ccd8a048
MDavid <2527132+MDylan@users.noreply.github.com>
2024-05-19 23:15:25 +0200

    Filter to congregation

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Filament\Facades\Filament;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,8 +14,23 @@ class City extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
-        'name'
+        'name',
+        'congregation_id'
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        //To generate unique slugs
+        static::creating(function ($city) {
+            $city->congregation_id = Filament::getTenant()->id;
+        });
+
+        static::addGlobalScope(function (Builder $builder) {
+            $builder->where('congregation_id', Filament::getTenant()->id);
+        });
+    }
 
     public function congregation(): BelongsTo
     {
